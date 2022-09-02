15120c412fd4e7ee14eff57a688d459a6b294fae
mohamad zareai <mohamadzareai@gmail.co>
2022-09-02 23:28:41 +0430

    Add Slice Redux-Toolkit, use *Payload and *Without Payload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Counter } from "./components/Counter/Counter";
 
 function App() {
   return (
-    <div>
-      <h>App</h>
-    </div>
+    <>
+      <Counter />
+    </>
   );
 }
 
